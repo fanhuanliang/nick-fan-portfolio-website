6774b1ec1d9cb0319e8f3256d86f8f1e2b498e66
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-26 10:09:47 -0700

    Implement the emailjs.

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -1,10 +1,60 @@
-import styleContact from '../../styles/Contact.module.css'
+import styleContact from "../../styles/Contact.module.css";
+import { useState } from "react";
+import emailjs from "emailjs-com";
+import Footer from "../Footer/Footer";
+import { serviceID, templateID, userID } from "./emailjs.config";
+import Notice from "./Notice"
 
-export default function contact() {
+export default function Contact() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [input, setInput] = useState({
+    name: "",
+    email: "",
+    message: "",
+    notification: ""
+  });
+
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setInput({
+      ...input,
+      [e.target.name]: value,
+    });
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+    setInput({
+      name: "",
+      email: "",
+      message: "",
+      notification: ""
+    });
+  }
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    if (!input.name || !input.email || !input.message) {
+        setInput({ ...input, notification: "Please fill out all the fields" });
+        setIsOpen(true);
+        return;     
+    } 
+      emailjs.sendForm(serviceID, templateID, e.target, userID).then(
+        (result) => {
+          setInput({ ...input, notification: "Thank you for your message!" });
+          setIsOpen(true);
+        },
+        (error) => {
+          setInput({ ...input, notification: "Something wrong with the API, please try again or reach out to me though LinkIn" });
+          setIsOpen(true);
+        }
+      );
+  };
+
   return (
     <div className={styleContact.contact} id="contact">
       <div className="container-fluid">
-        <div className="row">
+        <div className={styleContact.contact_row}>
           <div className="col-sm-12 col-md-12 text-center">
             <div className="contact-header">
               <h1>Contact</h1>
@@ -13,16 +63,20 @@ export default function contact() {
         </div>
 
         <div className="row">
-
           <div className="col-sm-6 col-md-6">
             <div className="contact-form-body">
-              <form>
+              <form onSubmit={sendEmail}>
                 <div className="row">
-
                   <div className="col-sm-6 col-md-6">
-                    <div className="lastname">
+                    <div className="name">
                       <h4>Name</h4>
-                      <input type="text" placeholder="Name" />
+                      <input
+                        type="text"
+                        placeholder="Name"
+                        name="name"
+                        value={input.name}
+                        onChange={handleInputChange}
+                      />
                     </div>
                   </div>
                 </div>
@@ -31,16 +85,27 @@ export default function contact() {
                   <div className="col-sm-12 col-md-12">
                     <div className="email">
                       <h4>Email</h4>
-                      <input type="text" placeholder="Email" />
+                      <input
+                        type="email"
+                        placeholder="Email"
+                        name="email"
+                        value={input.email}
+                        onChange={handleInputChange}
+                      />
                     </div>
                   </div>
                 </div>
 
                 <div className="row">
                   <div className="col-sm-12 col-md-12">
-                    <div className="message">
+                    <div className={styleContact.message}>
                       <h4>Message</h4>
-                      <textarea placeholder="Message"></textarea>
+                      <textarea
+                        placeholder="Message"
+                        name="message"
+                        value={input.message}
+                        onChange={handleInputChange}
+                      ></textarea>
                     </div>
                   </div>
                 </div>
@@ -59,6 +124,8 @@ export default function contact() {
           </div>
         </div>
       </div>
+      <Notice open={isOpen} onClose={handleClose} notification={input.notification} />
+      <Footer />
     </div>
   );
 }
