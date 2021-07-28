979268fec90153c4bbb3598a989873fc35500c91
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-27 21:09:30 -0700

    Delete react spring

--- components/Main/Main.js
+++ components/Main/Main.js
@@ -3,27 +3,11 @@ import React from "react";
 import Canvas from "../canvas/Canvas";
 import { Icon } from "semantic-ui-react";
 import { Link } from "react-scroll";
-import { useSpring, animated } from "react-spring";
 
 const Main = () => {
   React.useEffect(() => {
     Canvas();
   }, []);
-  const props1 = useSpring({
-    to: { opacity: 1 },
-    from: { opacity: 0 },
-    delay: 500,
-  });
-  const props2 = useSpring({
-    to: { opacity: 1 },
-    from: { opacity: 0 },
-    delay: 1000,
-  });
-  const props3 = useSpring({
-    to: { opacity: 1 },
-    from: { opacity: 0 },
-    delay: 1500,
-  });
 
   return (
     <div className={styleMain.main} id="main">
@@ -31,16 +15,16 @@ const Main = () => {
       <div className={styleMain.container_main}>
         <div className={styleMain.row}>
           <div className={styleMain.text_center}>
-            <animated.h1 style={props1} id={styleMain.hello}>
+            <h1 id={styleMain.hello}>
               Hello, I am
-            </animated.h1>
-            <animated.h1 style={props2} id={styleMain.name}>
+            </h1>
+            <h1 id={styleMain.name}>
               Huanliang Fan
-            </animated.h1>
+            </h1>
 
-            <animated.h1 style={props3} id={styleMain.person_description}>
+            <h1 id={styleMain.person_description}>
               Full Stack Software Engineer
-            </animated.h1>
+            </h1>
           </div>
         </div>
       </div>
