c1ea0ce4e3f8c712353772a39c72d35973e778eb
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-29 23:02:18 -0700

    implement tech component

---
+++ components/AboutMe/TechStacks.js
@@ -0,0 +1,15 @@
+// import Image from "next/image";
+
+// const TechStacks = ({ logoLink }) => {
+const TechStacks = () => {
+  return (
+    <div>
+      <div>
+        {/* <Image height={375} width={600} src={logoLink} alt="css" /> */}
+        hey
+      </div>
+    </div>
+  );
+}
+
+export default TechStacks
