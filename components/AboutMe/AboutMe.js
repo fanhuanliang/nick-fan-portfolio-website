c8b8c7df8d8048f1801739cc29e6210989389d95
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-29 23:01:55 -0700

    add techstacks component

--- components/AboutMe/AboutMe.js
+++ components/AboutMe/AboutMe.js
@@ -1,38 +1,50 @@
 import styleAbout from "../../styles/About.module.css";
 import Image from "next/image";
+import TechStacks from "./TechStacks";
+import { logo } from "./logoLink";
 
 export default function AboutMe() {
   return (
     <div className={styleAbout.about} id="about">
-      <div className={styleAbout.img}>
-        <Image
-          src="/images/selfie.png"
-          alt="selfie"
-          height={1000}
-          width={1000}
-        />
-      </div>
-      <div className={styleAbout.container_about}>
-        <div className="row">
-          <div className="col-sm-12 col-md-12 text-center">
-            <div className={styleAbout.about_header}>
-              <h1>About Me</h1>
+      <div className={styleAbout.wrapper}>
+        <div className={styleAbout.img}>
+          <Image
+            src="/images/selfie.png"
+            alt="selfie"
+            height={1000}
+            width={1000}
+          />
+        </div>
+        <div className={styleAbout.container_about}>
+          <div className="row">
+            <div className="col-sm-12 col-md-12 text-center">
+              <div className={styleAbout.about_header}>
+                <h1>About Me</h1>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className={styleAbout.description}>
+              <p className={styleAbout.paragraph}>
+                I am a full-stack software engineer with a background in Sales
+                and Business. I am pursuing my passion for tech through a
+                full-time opportunity as a Software Engineer. I like solving
+                problems and delivering clean code on time. Please contact me if
+                you are looking for a passionate Software Engineer.
+                <br></br>When I am not coding, I enjoy playing basketball,
+                cooking, traveling, and playing guitar.
+              </p>
             </div>
           </div>
         </div>
-
-        <div className="row">
-          <div className={styleAbout.description}>
-            <p className={styleAbout.paragraph}>
-              I am a full-stack software engineer with a background in Sales and
-              Business. I am pursuing my passion for tech through a full-time
-              opportunity as a Software Engineer. I like solving problems and
-              delivering clean code on time. Please contact me if you are
-              looking for a passionate Software Engineer.
-              <br></br>When I am not coding, I enjoy playing basketball,
-              cooking, traveling, and playing guitar.
-            </p>
-          </div>
+      </div>
+      <div className={styleAbout.tech}>
+        <h1>Technologies</h1>
+        <div>
+          {logo.map((item) => (
+            <TechStacks key={item.id} logoLink={item.link} />
+          ))}
         </div>
       </div>
     </div>
