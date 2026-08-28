ba34d4fa0d95f94f10a42f59862fa1dd980de1a1
Nick Fan <fanhuanliang@gmail.com>
2026-08-27 19:06:06 -0700

    Remove email link from footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import email from "../images/email.svg";
 import github from "../images/github.svg";
 import linkedIn from "../images/linkedin.svg";
 
@@ -14,11 +13,6 @@ const iconLinks = [
     label: "LinkedIn",
     icon: linkedIn,
   },
-  {
-    href: "mailto:nick.fan.sde@gmail.com",
-    label: "Email",
-    icon: email,
-  },
 ];
 
 export default function Footer() {
@@ -31,8 +25,8 @@ export default function Footer() {
               key={link.href}
               className="mx-[18px] inline-flex h-[34px] w-[34px] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[var(--text-primary)]"
               href={link.href}
-              target={link.href.startsWith("mailto:") ? undefined : "_blank"}
-              rel={link.href.startsWith("mailto:") ? undefined : "noopener noreferrer"}
+              target="_blank"
+              rel="noopener noreferrer"
               aria-label={link.label}
             >
               <Image className="h-[34px] w-[34px]" src={link.icon} alt="" />
